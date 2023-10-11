307c1f5f86e1c7b0d2c8637a2d92835700db9bae
Alen Toholj <alen.toholj@gmail.com>
2023-10-11 21:43:34 +0200

    CPU usage script

---
+++ Scripts/cpumonitor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cpu=$(top -b -n1 | awk '/Cpu/ {print $2}' | cut -d , -f1)
+hostname=$(hostname)
+
+if [[ ${cpu} -gt 75 ]]
+then
+    echo -e "Subject:CPU Usage!! \n\nThe cpu usage on ${hostname} machine is too high!! CPU usage is: ${cpu}%" | sendmail -t "alen.toholj@gmail.com" 2> /dev/null
+fi
